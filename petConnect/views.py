6bdf060ee817859ad0f7d60456df4dfca7b59856
Alisson Rodrigo <alissorodrigo098@gmail.com>
2023-10-20 13:24:22 -0300

    as

--- petConnect/views.py
+++ petConnect/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
 
-def inicial (request):
+# Create your views here.
+def inicial(request):
     return render(request, 'petConnect/pages/index.html')
