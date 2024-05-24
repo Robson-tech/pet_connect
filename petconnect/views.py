afda650e4e12de5119a27e9108880c6025d6c483
Alisson Rodrigo <alissorodrigo098@gmail.com>
2024-05-24 01:15:42 -0300

    n

--- petconnect/views.py
+++ petconnect/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-import json
-from .models import Usuario, Animais
+from django.shortcuts import redirect, render
+
+from .models import Animais, Usuario
 
 
 def index(request):
